dcfc53f8b73fc0ae0900b1d62563986b6db4728f
smlltt <samuel.liotta@gmail.com>
2022-10-30 19:27:33 +0100

    basic routing, login page design started

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import routes from "routes";
+import { Login } from "pages";
+import theme from "theme";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: routes.home,
+      element: <div>home</div>,
+    },
+    {
+      path: routes.login,
+      element: <Login />,
+    },
+  ]);
   return (
-<div>app test</div>
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   );
 }
 
---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    loginBg: {
+      100: "#c1beff",
+    },
+  },
+});
+
+export default theme;
