9d010ff6167284fcd57488330776de54013e5770
smlltt <samuel.liotta@gmail.com>
2022-10-31 16:45:03 +0100

    avatar

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Home, Moon, Grid, User, Mail, Bell, Search } from "react-feather";
-import { Box, Flex, Heading, HStack, Input } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Heading, HStack, Input } from "@chakra-ui/react";
 
 const Nav = () => {
   return (
@@ -27,7 +27,15 @@ const Nav = () => {
         <User />
         <Mail />
         <Bell />
-        <Box>John Doe</Box>
+        <HStack>
+          <Avatar
+            name={"Jane Doe"}
+            src={
+              "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+          />
+          <Box fontWeight={"semibold"}>Jane Doe</Box>
+        </HStack>
       </HStack>
     </Flex>
   );
