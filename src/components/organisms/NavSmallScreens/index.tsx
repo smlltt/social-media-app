2890165afb680e35cbe8451865b7fd2681f60690
smlltt <samuel.liotta@gmail.com>
2022-11-01 18:11:03 +0100

    nav small screens scrolling fix

--- src/components/organisms/NavSmallScreens/index.tsx
+++ src/components/organisms/NavSmallScreens/index.tsx
@@ -1,60 +1,30 @@
 import React, { FC } from "react";
-import { Box, Divider, Flex, Heading, Input, VStack } from "@chakra-ui/react";
-import { Menu } from "react-feather";
+import { Box, Divider, Input, VStack } from "@chakra-ui/react";
 import { Home, Moon, Grid, User, Mail, Bell, Search } from "react-feather";
 import { IconAndDescriptionSection } from "components/atoms";
 import { LeftBar } from "components/organisms";
 
-interface NavSmallScreensProps {
-  toggleMenu: () => void;
-  open: boolean;
-}
-
-const NavSmallScreens: FC<NavSmallScreensProps> = ({ toggleMenu, open }) => {
+const NavSmallScreens: FC = () => {
   return (
-    <Box>
-      <Flex
-        h={20}
-        justifyContent={"space-between"}
-        borderBottomWidth={1}
-        borderBottomStyle={"solid"}
-        borderBottomColor={"BlackAlpha"}
-        px={10}
-        alignItems={"center"}
-        position={"sticky"}
-        top={0}
-        background={"White"}
-      >
-        <Heading fontSize={24}>Social App</Heading>
-        <Box cursor={"pointer"}>
-          <Menu onClick={toggleMenu} />
-        </Box>
-      </Flex>
-      <VStack
-        alignItems={"start"}
-        display={open ? "flex" : "none"}
-        pb={10}
-        pt={5}
-      >
-        <IconAndDescriptionSection icon={<Home />} description={"Home"} />
-        <IconAndDescriptionSection icon={<Moon />} description={"Dark mode"} />
-        <IconAndDescriptionSection icon={<Grid />} description={"Menu"} />
-        <IconAndDescriptionSection
-          icon={<Search />}
-          description={<Input maxW={200} />}
-        />
-        <IconAndDescriptionSection icon={<User />} description={"Account"} />
-        <IconAndDescriptionSection icon={<Mail />} description={"Messages"} />
-        <IconAndDescriptionSection
-          icon={<Bell />}
-          description={"Notifications"}
-        />
-        <Box w={250} pb={10} pt={5}>
-          <Divider />
-          <LeftBar />
-        </Box>
-      </VStack>
-    </Box>
+    <VStack alignItems={"start"} pb={10} pt={5}>
+      <IconAndDescriptionSection icon={<Home />} description={"Home"} />
+      <IconAndDescriptionSection icon={<Moon />} description={"Dark mode"} />
+      <IconAndDescriptionSection icon={<Grid />} description={"Menu"} />
+      <IconAndDescriptionSection
+        icon={<Search />}
+        description={<Input maxW={200} />}
+      />
+      <IconAndDescriptionSection icon={<User />} description={"Account"} />
+      <IconAndDescriptionSection icon={<Mail />} description={"Messages"} />
+      <IconAndDescriptionSection
+        icon={<Bell />}
+        description={"Notifications"}
+      />
+      <Box w={250} pb={10} pt={5}>
+        <Divider />
+        <LeftBar />
+      </Box>
+    </VStack>
   );
 };
 
--- src/templates/AuthenticatedTemplate/index.tsx
+++ src/templates/AuthenticatedTemplate/index.tsx
@@ -1,7 +1,8 @@
 import React, { FC, ReactNode, useState } from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { Nav, NavSmallScreens, LeftBar } from "components/organisms";
-import { customScrollBarStyle } from "../../theme";
+import { customScrollBarStyle } from "theme";
+import { Menu } from "react-feather";
 
 interface AuthenticatedTemplateProps {
   children: ReactNode;
@@ -11,7 +12,10 @@ const AuthenticatedTemplate: FC<AuthenticatedTemplateProps> = ({
   children,
 }) => {
   const [smallMenuVisible, setSmallMenuVisible] = useState(false);
-  const displayContent = smallMenuVisible ? "none" : "flex";
+  const displayMenuSmallScreens = smallMenuVisible ? "block" : "none";
+  const toggleMenuSmallScreens = () => {
+    setSmallMenuVisible(!smallMenuVisible);
+  };
   return (
     <Box>
       {/*  large screens nav */}
@@ -23,29 +27,61 @@ const AuthenticatedTemplate: FC<AuthenticatedTemplateProps> = ({
       >
         <Nav />
       </Box>
-      {/*  small screens nav */}
-      <Box display={["block", "block", "block", "none"]}>
-        <NavSmallScreens
-          toggleMenu={() => setSmallMenuVisible(!smallMenuVisible)}
-          open={smallMenuVisible}
-        />
-      </Box>
+      {/*  small screens nav - if I put header and content in a single Box, the header's sticky position does not work in phones*/}
+      {/*  small nav header */}
       <Flex
+        h={20}
         justifyContent={"space-between"}
-        display={[displayContent, displayContent, displayContent, "flex"]}
+        borderBottomWidth={1}
+        borderBottomStyle={"solid"}
+        borderBottomColor={"BlackAlpha"}
+        px={10}
+        alignItems={"center"}
+        position={"sticky"}
+        top={0}
+        background={"White"}
+        zIndex={1}
+        display={["flex", "flex", "flex", "none"]}
       >
+        <Heading fontSize={24}>Social App</Heading>
+        <Box cursor={"pointer"}>
+          <Menu onClick={toggleMenuSmallScreens} />
+        </Box>
+      </Flex>
+      {/*  small nav content */}
+      <Box
+        display={[
+          displayMenuSmallScreens,
+          displayMenuSmallScreens,
+          displayMenuSmallScreens,
+          "none",
+        ]}
+        position={"sticky"}
+        top={20}
+        h={"calc(100vh - 80px)"}
+        overflowY={"hidden"}
+        _hover={{
+          overflowY: "auto",
+        }}
+        sx={customScrollBarStyle}
+        background={"white"}
+      >
+        <NavSmallScreens />
+      </Box>
+      {/*  dashboard content*/}
+      <Flex justifyContent={"space-between"}>
         <Box
           display={["none", "none", "none", "block"]}
           w={250}
           pb={10}
-          position={"sticky"}
-          top={20}
-          h={"calc(100vh - 100px)"}
           overflowY={"hidden"}
           _hover={{
             overflowY: "auto",
           }}
           sx={customScrollBarStyle}
+          position={"sticky"}
+          top={20}
+          h={"calc(100vh - 80px)"}
         >
           <LeftBar />
         </Box>
